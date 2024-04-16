183ad9aca7863db7dba06885237d701e7c4a571a
Dev Beto <147996450+JosejamesDev@users.noreply.github.com>
2024-04-16 01:42:13 -0300

    aaa

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let button = document.getElementById('no');
-let height = window.innerHeight - 100;
-let width = window.innerWidth - 100;
-
-button.addEventListener('mouseover', function() {
-  button.style.position = "absolute";
-    button.style.top = Math.random() * height + "px";
-      button.style.left = Math.random() * width + "px";
-      });
+let button=document.getElementById("no"),height=window.innerHeight-100,width=window.innerWidth-100;button.addEventListener("mouseover",function(){button.style.position="absolute",button.style.top=Math.random()*height+"px",button.style.left=Math.random()*width+"px"});
